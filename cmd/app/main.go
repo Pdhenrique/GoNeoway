8afcb09f937589c847bbfe7465e5c2f0a3210fcb
Pdhenrique <pedroschw.c@gmail.com>
2025-05-27 23:56:41 -0300

    refactor: moved db connection to connect package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
-	_ "github.com/lib/pq"
+	"github.com/Pdhenrique/GoNeoway/internal/db"
 )
 
 func main() {
-	DB_URL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
 
-	db, err := sql.Open("postgres", DB_URL)
+	conn, err := db.Connect()
 	if err != nil {
 		log.Fatal("ERROR connecting to the db", err)
-		panic(err)
 	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Error pinging the bank:", err)
-		panic(err)
-	}
+	defer conn.Close()
 
 	fmt.Println("API running and successfuly connected!")
 
---
+++ internal/db/connect.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func Connect() (*sql.DB, error) {
+	DB_URL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"))
+
+	db, err := sql.Open("postgres", DB_URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
