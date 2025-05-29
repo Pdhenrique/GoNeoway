2714f878acb969dc6290b34a75c849063567d12e
Pdhenrique <pedroschw.c@gmail.com>
2025-05-29 20:52:40 -0300

    refactor: move model to separated folder

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -8,21 +8,12 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/Pdhenrique/GoNeoway/pkg/model"
 )
 
-type Client struct {
-	CPF                   string     // 0 -> 18
-	PRIVATE               int        // 18 -> 30
-	INCOMPLETO            int        // 30 -> 43
-	DATA_ULTIMA_COMPRA    *time.Time //43 -> 65
-	TICKET_MEDIO          *float64   // 65 -> 87
-	TICKET_ULTIMA_COMPRA  *float64   // 87 -> 111
-	LOJA_MAIS_FREQUENTADA string     // 111 -> 131
-	LOJA_ULTIMA_COMPRA    string     // 131 -> 150
-}
-
-func Parse() ([]Client, error) {
-	var clients []Client
+func Parse(client string) ([]model.Client, error) {
+	var clients []model.Client
 
 	file, err := os.Open("base_teste.txt")
 	if err != nil {
@@ -70,7 +61,7 @@ func Parse() ([]Client, error) {
 		ticketMedio := parseNullableFloat(ticketMedioStr)
 		ticketUltima := parseNullableFloat(ticketUltimaStr)
 
-		clients = append(clients, Client{
+		clients = append(clients, model.Client{
 			CPF:                   cpf,
 			PRIVATE:               private,
 			INCOMPLETO:            incompleto,
@@ -95,11 +86,11 @@ func parseNullableFloat(arg string) *float64 {
 	if arg == "" || strings.ToUpper(arg) == "NULL" {
 		return nil
 	}
-	f, err := strconv.ParseFloat(arg, 64)
+	float, err := strconv.ParseFloat(arg, 64)
 	if err != nil {
 		return nil
 	}
-	return &f
+	return &float
 }
 
 // Transforma String em data e valida caso seja null
@@ -108,11 +99,11 @@ func parseNullableDate(arg string) *time.Time {
 		return nil
 	}
 	layout := "2006-01-02"
-	t, err := time.Parse(layout, arg)
+	time, err := time.Parse(layout, arg)
 	if err != nil {
 		return nil
 	}
-	return &t
+	return &time
 }
 
 // Verifica tamanho da
